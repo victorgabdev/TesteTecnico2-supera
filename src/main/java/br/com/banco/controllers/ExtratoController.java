0034b97ac67cc1a219f1bd5174efcc23c83f8ffe
Victor Gabriel <victorgabdev@gmail.com>
2023-07-18 11:00:27 -0300

    Criando paginação e dando update no endpoint

--- src/main/java/br/com/banco/controllers/ExtratoController.java
+++ src/main/java/br/com/banco/controllers/ExtratoController.java
@@ -3,9 +3,12 @@ package br.com.banco.controllers;
 import br.com.banco.entities.dtos.TransferenciaDTO;
 import br.com.banco.services.ExtratoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.time.LocalDate;
 import java.util.List;
@@ -18,15 +21,17 @@ public class ExtratoController {
     ExtratoService extratoService;
 
     // Levei em consideração que o numero da conta será o id da conta
-    @GetMapping
-    public ResponseEntity<List<TransferenciaDTO>> getExtrato(
-            @RequestParam(required = false) Long numeroConta,
+    @GetMapping("/{contaId}")
+    public ResponseEntity<Page<TransferenciaDTO>> getExtrato(
+            @PathVariable Long contaId,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate inicio,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fim,
-            @RequestParam(required = false) String nomeOperador
+            @RequestParam(required = false) String nomeOperador,
+            Pageable pageable
             ) {
 
-        // List<TransferenciaDTO> extrato = extratoService.obterExtrato(numeroConta, inicio, fim, nomeOperador);
+        Page<TransferenciaDTO> extrato = extratoService.obterExtrato(contaId, inicio, fim, nomeOperador, pageable);
+        return ResponseEntity.ok(extrato);
 
     }
 
